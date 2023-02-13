580aed90c92aa4839df27818f350e4b0b5dba83e
Николай Попов <nickpv@yandex.ru>
2023-02-14 02:58:51 +0700

    сделал нормальной пагинацию, добавил визуальных эффектов: тени при наведении

--- bicycle/bikes/models.py
+++ bicycle/bikes/models.py
@@ -8,6 +8,7 @@ class New_Bikes(models.Model):
     veloformat = models.CharField(max_length=15)
     picture = models.TextField()
     price = models.FloatField()
+    add_date = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'{self.brand} - {self.model}'
--- bicycle/bikes/views.py
+++ bicycle/bikes/views.py
@@ -1,11 +1,23 @@
+from django.core.paginator import Paginator
 from django.shortcuts import  get_object_or_404, redirect, render
 from .models import New_Bikes
+
+# постоянная с количеством постов на странице
+AMOUNT_POSTS = 15
 
 
 def index(request):
     all_details = New_Bikes.objects.all()
+    # создаем экземпляр класса Paginator и указываем количество товаров.
+    paginator = Paginator(all_details, AMOUNT_POSTS)
+    # получаем номер текущей страницы
+    page_number = request.GET.get('page')
+    # формируем список элементов текущей страницы
+    page_obj = paginator.get_page(page_number)
     context = {
+        'page_obj': page_obj,
         'all_details': all_details,
         'title': 'ГлаВВелосипеД',
+
     }
     return render(request, 'bikes/index.html', context)
